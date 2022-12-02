42304bab81b5d033f4613d8e8d43867ba2b06819
Harry W <hjohnw13@gmail.com>
2022-12-02 18:26:15 +0000

    Add java-jre17 features

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -8,6 +8,51 @@ echo "Java version: ${JAVA_VER}"
 # Make internal Docker IP address available to processes
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Check if startup command has -Dterminal.jline=false -Dterminal.ansi=true
+JLINE_ARGS=$(echo ${MODIFIED_STARTUP} | grep -o "\-Dterminal.jline=false -Dterminal.ansi=true")
+TIMEZONE_INUSE=$(echo ${MODIFIED_STARTUP} | grep -o "\-Duser.timezone=")
+
+# If Lower Xmx is enabled and above variable is empty, add the parameters to the startup command
+if [ "${LOWER_XMX}" = 1 ]; then
+    SERVER_MEMORY=$(expr $SERVER_MEMORY - 1024)
+    # If 512M server, use 256M
+    if [ "${SERVER_MEMORY}" == -512 ]; then SERVER_MEMORY=256; fi
+
+    # If 1GB server, use 512M
+    if [ "${SERVER_MEMORY}" == 0 ]; then SERVER_MEMORY=512; fi
+fi
+
+# Replace startup variables.
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# If Forge compatibility is enabled and above variable is empty, add the parameters to the startup command
+if [ "${FORGE_COMPATIBILITY}" = 1 ] && [ -z "${JLINE_ARGS}" ] && [ -z "${FORGE_VERSION}" ]; then
+    MODIFIED_STARTUP=$(echo "${MODIFIED_STARTUP}" | sed -E 's/-Xmx([0-9]+)M/& -Dterminal.jline=false -Dterminal.ansi=true/')
+    echo -e "\033[1;33mNOTE: \033[0mForge compatibility mode is enabled."
+fi
+
+# Log4j2 vulnerability workaround
+if [ "${LOG4J2_VULN_WORKAROUND}" = 1 ] && [ -z "${FORGE_VERSION}" ]; then
+    MODIFIED_STARTUP=$(echo "${MODIFIED_STARTUP}" | sed -E 's/-Xmx([0-9]+)M/& -Dlog4j2.formatMsgNoLookups=true/')
+    echo -e "\033[1;33mNOTE: \033[0mThe Log4j2 vulnerability workaround has been enabled. If you're running an unpatched server software build, remember to update ASAP as this workaround may be removed at any time, and is not effective in older versions of the game."
+fi
+
+# Forge 1.17.1+
+if [ -n "${FORGE_VERSION}" ]; then
+    MODIFIED_STARTUP="java -Xms128M -Xmx${SERVER_MEMORY}M -Dterminal.jline=false -Dterminal.ansi=true @libraries/net/minecraftforge/forge/${FORGE_VERSION}/unix_args.txt"
+fi
+
+# Timezone
+if [ ! "${TIMEZONE}" = "Default" ] && [ -z "${TIMEZONE_INUSE}" ] && [ ! -z "${TIMEZONE}" ]; then
+    MODIFIED_STARTUP=$(echo "${MODIFIED_STARTUP}" | sed -E 's/-Xmx([0-9]+)M/& -Duser.timezone=${TIMEZONE}/')
+fi
+
+# Aikar flags
+if [ "${AIKAR_FLAGS}" = 1 ]; then
+    MODIFIED_STARTUP=$(echo "${MODIFIED_STARTUP}" | sed -E 's/-Xmx([0-9]+)M/& -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true/')
+    echo -e "\033[1;33mNOTE: \033[0mEnabled Aikar's Flags"
+fi
+
 # Print startup command to console
 echo -e "\033[1;33mcustomer@sparkedhost:~\$\033[0m ${MODIFIED_STARTUP}"
 
