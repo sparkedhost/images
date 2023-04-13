6ccacf2ef04e2775164ad231d3fc3ae74f98b6f1
Griffindor <39884393+griffindor301@users.noreply.github.com>
2023-04-13 16:58:47 -0400

    Update entrypoint.sh

--- games/unturned/entrypoint.sh
+++ games/unturned/entrypoint.sh
@@ -3,20 +3,17 @@ sleep 1
 
 cd /home/container
 
-if [ "${STEAM_ACC}" == "GSLToken Not Set" ]; then
-    echo "game server token not set"
-fi
-
-./steam/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 1110390 +quit
-
 if [ "${GAME_AUTOUPDATE}" == "1" ]; then
     ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update 1110390 +quit
+else
+    echo -e "Not updating game server as auto update is off. Starting Server"
 fi
 
 if [ "${OPENMOD_AUTOUPDATE}" == "1" ]; then
     curl -s https://api.github.com/repos/openmod/OpenMod/releases/latest | jq -r ".assets[] | select(.name | contains(\"OpenMod.Unturned.Module\")) | .browser_download_url" | wget -i -
 	unzip -o -q OpenMod.Unturned.Module*.zip -d Modules && rm OpenMod.Unturned.Module*.zip
 fi
+
 
 if [ "${ROCKET_AUTOUPDATE}" == "1" ]; then
     cd /home/container
@@ -25,7 +22,8 @@ fi
 
 if [ "${USCRIPT_AUTOUPDATE}" == "1" ]; then
     wget https://trillionservers.com/unturned-egg/uScript.Unturned.zip
-	unzip -o -q uScript.Unturned.zip -d Modules && rm uScript.Unturned.zip
+    cd /home/container
+	unzip -o -q uScript.Unturned.zip -d Modules/uScript.Unturned && rm uScript.Unturned.zip
 fi
 
 mkdir -p Unturned_Headless_Data/Plugins/x86_64
@@ -34,10 +32,7 @@ cp -f steam/linux64/steamclient.so Unturned_Headless_Data/Plugins/x86_64/steamcl
 ulimit -n 2048
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Unturned_Headless_Data/Plugins/x86_64/
 
-
-
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-
 # Print startup command to console
 echo -e "\033[1;33mcustomer@sparkedhost:~\$\033[0m ${MODIFIED_STARTUP}"
 
