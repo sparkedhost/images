36d6a74e55974b9926095fb15c61df5eb2e86307
Raúl M <raul.m@sparkedhost.com>
2023-06-05 05:46:57 +0200

    Edit comment

--- games/rust/entrypoint.sh
+++ games/rust/entrypoint.sh
@@ -4,7 +4,7 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
-# Fixing Steam updating.
+# Rename SteamCMD directory
 if [ -d "./steamcmd" ]; then
     mv steamcmd steam
 fi
