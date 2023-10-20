e0c50bca973318df5dc4b6be0d46fe708eb14487
Griffindor <39884393+griffindor301@users.noreply.github.com>
2023-10-20 04:51:04 -0400

    Update entrypoint.sh

--- games/unturned-mh/entrypoint.sh
+++ games/unturned-mh/entrypoint.sh
@@ -3,6 +3,15 @@ sleep 1
 
 cd /home/container
 
+# Fix SteamCMD Warnings
+if [ ! -f "./steam/linux32/steamservice.so" ]; then
+    cd /home/container/steam/linux32
+    cp steamclient.so steamservice.so
+    cp steamclient.so libSDL3.so.0
+    echo "SteamCMD Fix Deployed"
+    cd /home/container
+fi
+
 if [ "${GAME_AUTOUPDATE}" != "0" ]; then
     ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +login anonymous +force_install_dir /home/container +app_update 1110390 +quit
 else
@@ -15,6 +24,36 @@ fi
 
 mkdir -p Unturned_Headless_Data/Plugins/x86_64
 cp -f steam/linux64/steamclient.so Unturned_Headless_Data/Plugins/x86_64/steamclient.so
+
+if [ "${UPANEL}" == "Vanilla" ]; then
+    cd /home/container/Modules
+    mkdir uPanelLoader
+    cd uPanelLoader
+    wget https://upanel.one/api/data/loader/Module
+    unzip -o Module
+    rm -r Module
+    cd /home/container
+fi
+
+if [ "${UPANEL}" == "RocketMod" ]; then
+    cd /home/container/Servers/unturned/Rocket
+    wget https://upanel.one/api/data/loader/Rocket
+    unzip -o Rocket
+    cp uPanelLoader.dll /Plugins/
+    rm -r Rocket
+    rm -r uPanelLoader.dll
+    cd /home/container
+fi
+
+if [ "${UPANEL}" == "OpenMod" ]; then
+    cd /home/container/Servers/unturned/OpenMod/plugins
+    wget https://upanel.one/api/data/loader/Openmod
+    unzip -o Openmod
+    mv Libraries/* .
+    rm -r Openmod
+    rm -r Libraries
+    cd /home/container
+fi
 
 ulimit -n 2048
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Unturned_Headless_Data/Plugins/x86_64/
