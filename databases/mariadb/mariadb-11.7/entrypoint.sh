51939fbad605fd76723600c00e737aa54ccef9bd
Lachlan Roche <lachlan.r@sparkedhost.com>
2025-02-12 17:44:46 +0000

    MariaDB 11.7 w/ web-ui source

--- databases/mariadb/mariadb-11.7/entrypoint.sh
+++ databases/mariadb/mariadb-11.7/entrypoint.sh
@@ -1,114 +1,102 @@
 #!/bin/bash
 cd /home/container
 
-# Function to show usage
-usage() {
-  echo "Usage: $0 --pma-port <port> --db-port <port> --web-enable <0|1> [--upgrade-db]"
-  exit 1
-}
-
-# Parse arguments
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    --pma-port) PMA_PORT="$2"; shift ;;
-    --db-port) DB_PORT="$2"; shift ;;
-    --web-enable) WEB_ENABLE="$2"; shift ;;
-    *) usage ;;
-  esac
-  shift
-done
-
-# Check required arguments
-if [ -z "$PMA_PORT" ] || [ -z "$DB_PORT" ] || [ -z "$WEB_ENABLE" ]; then
-  usage
-fi
-
-if [ "$WEB_ENABLE" != "0" ] && [ "$WEB_ENABLE" != "1" ]; then
-  usage
-fi
-
-# Make internal Docker IP address available to processes.
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
+# Define MariaDB variables
+export MARIADB_SOCKET="/home/container/run/mysqld/mysqld.sock"
+export MARIADB_DATADIR="/home/container/mysql"
+export MARIADB_TMPDIR="/tmp"
+export MARIADB_LC_MESSAGES_DIR="/home/container/mysql/lc/"
+export MARIADB_LOG_DIR="/home/container/log/mysql"
+export SERVER_PORT="${SERVER_PORT_ENV:-3306}"
+export WEB_UI_PORT="${WEB_UI_PORT_ENV:-8080}"
+## WEB_UI_ENABLED is unused
+export WEB_UI_ENABLED=""
+export MAX_CONNECTIONS="${MAX_CONNECTIONS_ENV:-500}"
 
 # Determine correct executables to use based on MariaDB version
 if which mariadb > /dev/null ; then
-  # MariaDB 11.x or later
-  MARIADB_EXECUTABLE="mariadb"
+  MARIADB_EXECUTABLE="mariadbd"
   MARIADB_INSTALLDB_EXECUTABLE="mariadb-install-db"
   MARIADB_UPGRADE_EXECUTABLE="mariadb-upgrade"
 else
-  # Likely MariaDB 10.x or earlier (assuming mariadbd-safe is not available)
-  MARIADB_EXECUTABLE="mysql"
+  MARIADB_EXECUTABLE="mysqld"
   MARIADB_INSTALLDB_EXECUTABLE="mysql_install_db"
   MARIADB_UPGRADE_EXECUTABLE="mysql_upgrade"
 fi
 
-# Function to get MariaDB version
-get_mariadb_version() {
-    MARIADB_VERSION=$($MARIADB_EXECUTABLE --version | grep -oP '(?<=Ver )[^ ]+')
+# Ensure required folders exist
+mkdir -p /home/container/run/mysqld
+mkdir -p /home/container/run/php
+mkdir -p /home/container/run/php/log
+mkdir -p $MARIADB_LOG_DIR
+mkdir -p $MARIADB_DATADIR
+mkdir -p /home/container/mysql/lc/
+mkdir -p /home/container/etc
+mkdir -p /home/container/etc/php-fpm
+mkdir -p /home/container/etc/caddy/
+mkdir -p /usr/share/mysql
+
+# Generate Caddyfile
+generate_caddyfile() {
+  cat <<EOF > /home/container/etc/caddy/Caddyfile
+:$WEB_UI_PORT {
+    root * /var/www/phpmyadmin
+    file_server
+
+    @forbidden {
+        path_regexp forbidden ^/(doc|sql|setup)/
+    }
+    respond @forbidden 403
+
+    request_body {
+        max_size 10GB
+    }
+
+    header {
+        X-Content-Type-Options "nosniff"
+        X-XSS-Protection "1; mode=block"
+        X-Robots-Tag "none"
+        Content-Security-Policy "frame-ancestors 'self'"
+        X-Frame-Options "DENY"
+        Referrer-Policy "same-origin"
+    }
+
+    php_fastcgi 127.0.0.1:9999 {
+            env PHP_VALUE "upload_max_filesize = 10G \n post_max_size=10G"
+    }
+
+    @htaccess {
+        path_regexp htaccess /\.ht
+    }
+ 
+EOF
 }
 
-# Call the function on startup
-get_mariadb_version
+generate_caddyfile
 
-# Setup NSS Wrapper - Some variables already set in the Dockerfile
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
-envsubst < /passwd.template > ${NSS_WRAPPER_PASSWD}
-envsubst < /group.template > ${NSS_WRAPPER_GROUP}
-if [ -f /usr/lib/libnss_wrapper.so ]; then
-  export LD_PRELOAD=/usr/lib/libnss_wrapper.so
-else
-  export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libnss_wrapper.so
+# if ! mysqladmin -u root --socket="$MARIADB_SOCKET" ping > /dev/null 2>&1; then
+#   echo "Error: MariaDB failed to start within $TIMEOUT seconds."
+#   exit 1
+# fi
+
+# Optional: Perform database upgrade
+# sleep 5
+# if $MARIADB_UPGRADE_EXECUTABLE --check-if-upgrade-is-needed; then
+#     echo "Upgrading MariaDB..."
+#     $MARIADB_UPGRADE_EXECUTABLE -u container
+# fi
+
+
+if [ ! -d "$MARIADB_DATADIR/mysql" ]; then
+  echo "Initializing MariaDB..."
+  $MARIADB_INSTALLDB_EXECUTABLE \
+    --socket="$MARIADB_SOCKET" \
+    --datadir="$MARIADB_DATADIR" \
+    --tmpdir="$MARIADB_TMPDIR" \
+    --lc-messages-dir="$MARIADB_LC_MESSAGES_DIR"
 fi
 
-# Ensure required folders exist
-mkdir -p $HOME/run/mysqld
-mkdir -p $HOME/log/mysql
-mkdir -p $HOME/mysql
-mkdir -p $HOME/etc
-mkdir -p $HOME/etc/nginx/
-mkdir -p $HOME/usr/share/mysql
 
-# Only run install db if the database is not setup
-if [ ! -f $HOME/mysql/mysql.frm ] ; then
-  echo "Database not setup - exiting with error"
-  exit 1
-fi
-
-# Automatically run the functions that --normal would trigger
-# Start web server if WEB_ENABLE is set to 1
-if [ "$WEB_ENABLE" -eq 1 ]; then
-  # Start nginx in the background
-  nginx -g "daemon off;" -c /etc/nginx/nginx.conf -p $HTTP_PORT &
-  echo "Web server started."
-else
-  echo "WEB_ENABLE is not set to 1. Skipping web server startup."
-fi
-
-# Start MariaDB in the background
-$MARIADB_EXECUTABLE --socket=$HOME/run/mysqld/mysqld.sock --host-cache-size=0 --skip-name-resolve --pid-file=$HOME/run/mysqld/mysqld.pid --basedir=/usr --expire_logs_days=10 --character-set-server=utf8mb4 --character-set-collations=utf8mb4=uca1400_ai_ci --port=$DB_PORT --socket=$HOME/run/mysqld/mysqld.sock --basedir=/usr --datadir=$HOME/mysql --tmpdir=/tmp --lc_messages_dir=$HOME/usr/share/mysql --lc_messages=en_US --skip-external-locking --bind-address=0.0.0.0 --max_connections=100 --connect_timeout=5 --wait_timeout=600 --max_allowed_packet=16M --thread_cache_size=128 --sort_buffer_size=4M --bulk_insert_buffer_size=16M --tmp_table_size=32M --max_heap_table_size=32M --myisam_recover_options=BACKUP --key_buffer_size=128M --table_open_cache=400 --myisam_sort_buffer_size=512M --concurrent_insert=2 --read_buffer_size=2M --read_rnd_buffer_size=1M --query_cache_limit=128K --query_cache_size=64M --general_log_file=$HOME/log/mysql/mysql.log --slow_query_log_file=$HOME/log/mysql/mariadb-slow.log --long_query_time=10 --expire_logs_days=10 --max_binlog_size=100M --default_storage_engine=InnoDB --innodb_buffer_pool_size=256M --innodb_log_buffer_size=8M --innodb_file_per_table=1 --innodb_open_files=400 --innodb_io_capacity=400 --innodb_flush_method=O_DIRECT --bind-address=0.0.0.0 --socket=$HOME/run/mysqld/mysqld.sock --host-cache-size=0 --skip-name-resolve --pid-file=$HOME/run/mysqld/mysqld.pid --basedir=/usr --expire_logs_days=10 --character-set-server=utf8mb4 --character-set-collations=utf8mb4=uca1400_ai_ci &
-
-PID=$!
-
-sleep 5
-
-# Check if upgrade is needed and perform upgrade if necessary
-if $MARIADB_UPGRADE_EXECUTABLE --check-if-upgrade-is-needed; then
-  echo "Upgrading MariaDB..."
-  $MARIADB_UPGRADE_EXECUTABLE -u container
-fi
-
-# Show MariaDB logs
-tail -f /home/container/log/mysql/mysql.log /home/container/log/mysql/mariadb-slow.log &
-
-# Replace Startup Variables
-MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
-# Run the Server
-eval ${MODIFIED_STARTUP}
-
-# Wait for the server to stop
-wait $PID > /dev/null 2>&1
+# Start supervisord
+/usr/bin/supervisord -c /supervisord.conf
+echo "MariaDB stopped."
