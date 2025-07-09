b4ac04aecf43d72a0602544aff4be729a54a4842
Griffindor <39884393+griffindor301@users.noreply.github.com>
2025-07-09 09:07:48 -0400

    Updated CS2 Entrypoint.

--- games/cs2/entrypoint.sh
+++ games/cs2/entrypoint.sh
@@ -3,8 +3,7 @@ sleep 1
 cd /home/container
 
 if [ "${AUTO_UPDATE}" == "1" ]; then 
-    ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
-else
+    ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quitelse
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
