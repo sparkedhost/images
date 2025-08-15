76c3d7a72420504df7653990a6046463d58ac275
griffindor301 <griffhoover@outlook.com>
2025-08-15 14:38:02 -0400

    Updated Rust Image

--- games/rust/entrypoint.sh
+++ games/rust/entrypoint.sh
@@ -2,15 +2,18 @@
 cd /home/container
 
 # Make internal Docker IP address available to processes.
-export INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 
-# Rename SteamCMD directory
+# Fixing Steam updating.
 if [ -d "./steamcmd" ]; then
     mv steamcmd steam
 fi
 
-# Updating Rust Server
-./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) +quit
+if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
+	./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) +quit
+else
+    echo -e "Not updating game server as auto update was set to 0. Starting Server"
+fi
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
@@ -19,7 +22,7 @@ echo -e "\033[1;33mcustomer@apollopanel:~\$\033[0m ${MODIFIED_STARTUP}"
 if [ -f CARBON_FLAG ] || [ "${FRAMEWORK}" == "carbon" ]; then
     # Carbon: https://github.com/CarbonCommunity/Carbon.Core
     echo "Updating Carbon Modding Framework..."
-    curl -sSL "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | tar zx
+    curl -sL "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | tar zx
     echo "Done updating Carbon Modding Framework!"
 
     # backward compatibility
@@ -32,7 +35,7 @@ if [ -f CARBON_FLAG ] || [ "${FRAMEWORK}" == "carbon" ]; then
 elif [ -f OXIDE_FLAG ] || [ "${FRAMEWORK}" == "oxide" ]; then
     # Oxide: https://github.com/OxideMod/Oxide.Rust
     echo "Updating uMod..."
-    curl -sSL "https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" > umod.zip
+    curl -sL "https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" > umod.zip
     unzip -o -q umod.zip
     rm umod.zip
     echo "Done updating uMod!"
@@ -48,4 +51,4 @@ fi
 export LD_LIBRARY_PATH=$(pwd)/RustDedicated_Data/Plugins/x86_64:$(pwd)
 
 # Run the Server
-node /wrapper.js "${MODIFIED_STARTUP}"
+/wrapper/wrapper.js "${MODIFIED_STARTUP}"
--- games/rust/wrapper.js
+++ games/rust/wrapper.js
@@ -1,134 +1,140 @@
 #!/usr/bin/env node
 
+var startupCmd = "";
 const fs = require("fs");
-const path = require("path");
-const { exec } = require("child_process");
-
-const logFilePath = path.join(__dirname, "latest.log");
-fs.writeFile(logFilePath, "", (err) => {
-    if (err) console.log("Callback error in appendFile:" + err);
+fs.writeFile("latest.log", "", (err) => {
+	if (err) console.log("Callback error in appendFile:" + err);
 });
 
-let startupCmd = "";
-const args = process.argv.splice(process.execArgv.length + 2);
-for (let i = 0; i < args.length; i++) {
-    startupCmd += args[i] + (i === args.length - 1 ? "" : " ");
+var args = process.argv.splice(process.execArgv.length + 2);
+for (var i = 0; i < args.length; i++) {
+	if (i === args.length - 1) {
+		startupCmd += args[i];
+	} else {
+		startupCmd += args[i] + " ";
+	}
 }
 
 if (startupCmd.length < 1) {
-    console.log("Error: Please specify a startup command.");
-    process.exit(1);
+	console.log("Error: Please specify a startup command.");
+	process.exit();
 }
 
 const seenPercentage = {};
-function filter(data) {
-    const str = data.toString();
-    if (str.startsWith("Loading Prefab Bundle ")) {
-        const percentage = str.substr("Loading Prefab Bundle ".length);
-        if (seenPercentage[percentage]) return;
-        seenPercentage[percentage] = true;
-    }
 
-    console.log(str);
+function filter(data) {
+	const str = data.toString();
+	if (str.startsWith("Loading Prefab Bundle ")) { // Rust seems to spam the same percentage, so filter out any duplicates.
+		const percentage = str.substr("Loading Prefab Bundle ".length);
+		if (seenPercentage[percentage]) return;
+
+		seenPercentage[percentage] = true;
+	}
+
+	console.log(str);
 }
 
+var exec = require("child_process").exec;
 console.log("Starting Rust...");
 
-let exited = false;
+var exited = false;
 const gameProcess = exec(startupCmd);
-gameProcess.stdout.on("data", filter);
-gameProcess.stderr.on("data", filter);
-gameProcess.on("exit", function (code, signal) {
-    exited = true;
-    if (code) {
-        console.log("Main game process exited with code " + code);
-    }
+gameProcess.stdout.on('data', filter);
+gameProcess.stderr.on('data', filter);
+gameProcess.on('exit', function (code, signal) {
+	exited = true;
+
+	if (code) {
+		console.log("Main game process exited with code " + code);
+		// process.exit(code);
+	}
 });
 
 function initialListener(data) {
-    const command = data.toString().trim();
-    if (command === "quit") {
-        gameProcess.kill("SIGTERM");
-    } else {
-        console.log(`Unable to run "${command}" due to RCON not being connected yet.`);
-    }
+	const command = data.toString().trim();
+	if (command === 'quit') {
+		gameProcess.kill('SIGTERM');
+	} else {
+		console.log('Unable to run "' + command + '" due to RCON not being connected yet.');
+	}
 }
-
 process.stdin.resume();
 process.stdin.setEncoding("utf8");
-process.stdin.on("data", initialListener);
+process.stdin.on('data', initialListener);
 
-process.on("exit", function () {
-    if (!exited) {
-        console.log("Received request to stop the process, stopping the game...");
-        gameProcess.kill("SIGTERM");
-    }
+process.on('exit', function (code) {
+	if (exited) return;
+
+	console.log("Received request to stop the process, stopping the game...");
+	gameProcess.kill('SIGTERM');
 });
 
-function poll() {
-    if (!process.env.RCON_PORT || !process.env.RCON_PASS) {
-        console.error("Missing RCON_PORT or RCON_PASS in environment.");
-        process.exit(1);
-    }
+var waiting = true;
+var poll = function () {
+	function createPacket(command) {
+		var packet = {
+			Identifier: -1,
+			Message: command,
+			Name: "WebRcon"
+		};
+		return JSON.stringify(packet);
+	}
 
-    const serverHostname = process.env.RCON_IP || "localhost";
-    const serverPort = process.env.RCON_PORT;
-    const serverPassword = process.env.RCON_PASS;
-    const WebSocket = require("ws");
+	var serverHostname = process.env.RCON_IP ? process.env.RCON_IP : "localhost";
+	var serverPort = process.env.RCON_PORT;
+	var serverPassword = process.env.RCON_PASS;
+	var WebSocket = require("ws");
+	var ws = new WebSocket("ws://" + serverHostname + ":" + serverPort + "/" + serverPassword);
 
-    function createPacket(command) {
-        return JSON.stringify({
-            Identifier: -1,
-            Message: command.trim(),
-            Name: "WebRcon",
-        });
-    }
+	ws.on("open", function open() {
+		console.log("Connected to RCON. Generating the map now. Please wait until the server status switches to \"Running\".");
+		waiting = false;
 
-    const ws = new WebSocket(`ws://${serverHostname}:${serverPort}/${serverPassword}`);
-    let waiting = true;
+		// Hack to fix broken console output
+		ws.send(createPacket('status'));
 
-    ws.on("open", () => {
-        console.log('Connected to RCON. Please wait until the server status switches to "Running".');
-        waiting = false;
+		process.stdin.removeListener('data', initialListener);
+		gameProcess.stdout.removeListener('data', filter);
+		gameProcess.stderr.removeListener('data', filter);
+		process.stdin.on('data', function (text) {
+			ws.send(createPacket(text));
+		});
+	});
 
-        ws.send(createPacket("status"));
+	ws.on("message", function (data, flags) {
+		try {
+			var json = JSON.parse(data);
+			if (json !== undefined) {
+				if (json.Message !== undefined && json.Message.length > 0) {
+					console.log(json.Message);
+					const fs = require("fs");
+					fs.appendFile("latest.log", "\n" + json.Message, (err) => {
+						if (err) console.log("Callback error in appendFile:" + err);
+					});
+				}
+			} else {
+				console.log("Error: Invalid JSON received");
+			}
+		} catch (e) {
+			if (e) {
+				console.log(e);
+			}
+		}
+	});
 
-        process.stdin.removeListener("data", initialListener);
-        gameProcess.stdout.removeListener("data", filter);
-        gameProcess.stderr.removeListener("data", filter);
+	ws.on("error", function (err) {
+		waiting = true;
+		console.log("Waiting for RCON to come up...");
+		setTimeout(poll, 5000);
+	});
 
-        process.stdin.on("data", function (text) {
-            ws.send(createPacket(text));
-        });
-    });
+	ws.on("close", function () {
+		if (!waiting) {
+			console.log("Connection to server closed.");
 
-    ws.on("message", function (data) {
-        try {
-            const json = JSON.parse(data);
-            if (json?.Message) {
-                console.log(json.Message);
-                fs.appendFile(logFilePath, "\n" + json.Message, (err) => {
-                    if (err) console.log("Callback error in appendFile:" + err);
-                });
-            }
-        } catch (e) {
-            console.log("Error parsing JSON message:", e);
-        }
-    });
-
-    ws.on("error", function () {
-        waiting = true;
-        console.log("Waiting for RCON to come up...");
-        setTimeout(poll, 5000);
-    });
-
-    ws.on("close", function () {
-        if (!waiting) {
-            console.log("Connection to server closed.");
-            exited = true;
-            process.exit();
-        }
-    });
+			exited = true;
+			process.exit();
+		}
+	});
 }
-
 poll();
