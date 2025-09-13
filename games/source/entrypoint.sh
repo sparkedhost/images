369a0439ead78bf1f7f322b2e4c86fec78cb72c7
griffindor301 <griffhoover@outlook.com>
2025-09-13 16:27:25 -0400

    Fix

--- games/source/entrypoint.sh
+++ games/source/entrypoint.sh
@@ -2,14 +2,14 @@ sleep 1
 
 cd /home/container
 
-if [ "${AUTO_UPDATE}" == "1" ]; then 
+if [ "${AUTO_UPDATE}" == "1" ] && [ -n "${SRCDS_BETAID}" ]; then 
     if [ -d "./steamcmd" ]; then
-    ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+    ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
     fi
     if [ -d "./steam" ]; then
-    ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+    ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
     fi
-else
+elif [ "${AUTO_UPDATE}" != "1" ]; then
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
