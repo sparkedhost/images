8db4202c5ee6ef8c553eb0e1a1496b1ffe37029d
griffindor301 <griffhoover@outlook.com>
2026-04-28 06:39:10 -0400

    Updated Entrypoint to include steamcmd S&BOX

--- games/sbox/entrypoint.sh
+++ games/sbox/entrypoint.sh
@@ -4,6 +4,18 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
+
+if [ "${AUTO_UPDATE}" == "1" ] && [ -n "${SRCDS_APPID}" ]; then 
+    if [ -d "./steamcmd" ]; then
+    ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ "${VALIDATE}" == "1" ]] && printf %s "validate" ) +quit
+    fi
+    if [ -d "./steam" ]; then
+    ./steam/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ "${VALIDATE}" == "1" ]] && printf %s "validate" ) +quit
+    fi
+elif [ "${AUTO_UPDATE}" != "1" ]; then
+    echo -e "Not updating game server as auto update was set to 0. Starting Server"
+fi
+
 # Replace Startup variables.
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo "customer@apollopanel:~# ${MODIFIED_STARTUP}"
