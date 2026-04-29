1c013f47cd51bb66fe5a4dadb299f20e87381126
Linux123123 <me@linux123123.com>
2026-04-29 03:53:59 +0300

    feat: add migration

--- games/sbox/entrypoint.sh
+++ games/sbox/entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+source /spark-utils.sh
 cd /home/container
+
+function migrate() {
+    echo "Migrating from old sbox-public directory to new sbox directory..."
+    rm -rf /home/container/data /home/container/config
+    mv ./sbox-public/game/data data
+    mv ./sbox-public/game/config config
+    rm -rf sbox-public .dotnet .local .cache .nuget .wget-hsts Steam
+    
+    SERVER_HOME=/home/container
+    install_steamcmd
+    ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ "${VALIDATE}" == "1" ]] && printf %s "validate" ) +quit
+}
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
+if [ -d "./sbox-public" ]; then
+    migrate
+fi
 
 if [ "${AUTO_UPDATE}" == "1" ] && [ -n "${SRCDS_APPID}" ]; then 
     if [ -d "./steamcmd" ]; then
