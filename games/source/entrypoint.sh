d31aef4d6174776e971fc497bd8894a5e025c355
barrelltitor <barrelltitor@protonmail.com>
2026-05-06 22:59:02 +0300

    feat: conan linux

--- games/source/entrypoint.sh
+++ games/source/entrypoint.sh
@@ -1,5 +1,11 @@
 sleep 1
 
+MOD_FILE=modlist.html
+SERVER_HOME=/home/container
+STEAMCMD_ATTEMPTS=${STEAMCMD_ATTEMPTS:-3}
+
+source /spark-utils.sh
+
 dst_mod_fix() {
     [[ $SRCDS_APPID != 343050 ]] && return
     cp -f  steamcmd/linux64/steamclient.so  bin64/lib64/steamclient.so
@@ -20,8 +26,4 @@ fi
 
 dst_mod_fix
 
-MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-
-echo -e "\033[1;33mcustomer@apollopanel:~\$\033[0m ${MODIFIED_STARTUP}"
-
-eval ${MODIFIED_STARTUP}
+startup_game
