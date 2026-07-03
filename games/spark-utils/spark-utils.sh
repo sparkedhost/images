434a6b01ad49916c995ae69cbbd309cb4978adea
barrelltitor <barrelltitor@protonmail.com>
2026-07-03 19:27:26 +0300

    fix: missing mod install error

--- games/spark-utils/spark-utils.sh
+++ games/spark-utils/spark-utils.sh
@@ -155,9 +155,9 @@ RunSteamCMD() { #[Input: int server=0 mod=1; int id]
                 echo -e "Your Egg, or your client's server, is not configured with valid Steam credentials."
                 echo -e "Either the username/password is wrong, or Steam Guard is not properly configuredaccording to this egg's documentation/README."
                 exit 1
-            elif [[ $1 == 1 ]] && [[ -n $(grep -i "Failed updating depot" "${steamcmd_log}") ]] && [[ -n $(grep -i "File Not Found\|Missing game files" "${steamcmd_log}") ]]; then # Corrupt or incomplete workshop download
-                echo -e "[UPDATE]: Workshop download is missing files. Removing cached mod files and retrying..."
-                rm -rf "${workshop_dir}/content/${GAME_ID}/$2"
+            elif [[ $1 == 1 ]] && [[ -n $(grep -Ei "\[AppID ${GAME_ID}\].*Update canceled: Failed updating depot [0-9]+ while reading chunk \"[0-9a-f]+\" \(File Not Found\) \(Missing game files\) \".*steamapps/workshop/content/${GAME_ID}/[0-9]+/" "${steamcmd_log}") ]]; then # Corrupt Steam Workshop app manifest
+                echo -e "[UPDATE]: Workshop download is missing game files. Removing cached workshop manifest and retrying..."
+                rm -f "${workshop_dir}/appworkshop_${GAME_ID}.acf"
             elif [[ -n $(grep -i "Download item" "${steamcmd_log}") ]]; then # Steam account does not own base game for mod downloads, or unknown
                 echo -e "[UPDATE]: Cannot download mod - Download failed"
                 echo -e "While unknown, this error is likely due to your host's Steam account not owning the base game."
