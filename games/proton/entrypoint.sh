f3b4476c1aa80f095ed9b4aa1177170214fa5c28
barrelltitor <barrelltitor@protonmail.com>
2026-08-21 14:49:43 +0300

    feat: have proton use our spark utils

--- games/proton/entrypoint.sh
+++ games/proton/entrypoint.sh
@@ -1,5 +1,7 @@
 sleep 1
 
+source /spark-utils.sh
+
 if [ -f "/usr/local/bin/proton" ]; then
     if [ ! -z ${SRCDS_APPID} ]; then
 	mkdir -p /home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}
@@ -19,13 +21,10 @@ fi
 cd /home/container || exit 1
 
 if [ "${AUTO_UPDATE}" == "1" ]; then 
-    ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformBitness 64 +force_install_dir /home/container +login anonymous $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+    ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
 else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
-MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-
-echo -e "\033[1;33mcustomer@apollopanel:~\$\033[0m ${MODIFIED_STARTUP}"
-
-eval ${MODIFIED_STARTUP}
+game_pre_startup
+startup_game
