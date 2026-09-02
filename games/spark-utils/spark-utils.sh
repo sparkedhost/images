015cd7e214be888a9ea6f48bd9174599266f0b45
barrelltitor <barrelltitor@protonmail.com>
2026-09-02 22:18:50 +0300

    fix: dont delete on vanilla

--- games/spark-utils/spark-utils.sh
+++ games/spark-utils/spark-utils.sh
@@ -951,11 +951,6 @@ cleanup_valheim_framework(){
         bepinex)
             rm -f -- BepInEx/plugins/ValheimPlus.dll BepInEx/config/valheim_plus.cfg .apollo/valheim_plus_version
             ;;
-        vanilla)
-            rm -rf -- BepInEx doorstop_libs unstripped_corlib
-            rm -f -- doorstop_config.ini start_game_bepinex.sh start_server_bepinex.sh run_bepinex.sh
-            rm -f -- .apollo/bepinex_version .apollo/valheim_plus_version
-            ;;
     esac
 }
 
