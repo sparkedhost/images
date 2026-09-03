d94e0554d9f4d6f3273cf82353d9a94fe08fa994
barrelltitor <barrelltitor@protonmail.com>
2026-09-04 00:06:04 +0300

    fix: only login if steam credentials present

--- games/proton/entrypoint.sh
+++ games/proton/entrypoint.sh
@@ -9,7 +9,12 @@ fi
 cd /home/container || exit 1
 
 if [ "${AUTO_UPDATE}" == "1" ]; then
-    ./steamcmd/steamcmd.sh +force_install_dir /home/container "+login \"${STEAM_USER}\" \"${STEAM_PASS}\"" $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+    STEAM_LOGIN="+login anonymous"
+    if [[ -n "${STEAM_USER:-}" && -n "${STEAM_PASS:-}" ]]; then
+        STEAM_LOGIN="+login \"${STEAM_USER}\" \"${STEAM_PASS}\""
+    fi
+
+    ./steamcmd/steamcmd.sh +force_install_dir /home/container "${STEAM_LOGIN}" $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
 else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
