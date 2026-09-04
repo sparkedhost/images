f90d33a2f4bb4954b357469e24248d7e187c82cb
Barrell Titor <108460132+barrelltitor@users.noreply.github.com>
2026-09-04 20:21:01 +0300

    fix: add mesa vulkan drivers + move setup to spark utils (#99)

--- games/proton/entrypoint.sh
+++ games/proton/entrypoint.sh
@@ -2,26 +2,19 @@ sleep 1
 
 source /spark-utils.sh
 
-if [ -f "/usr/local/bin/proton" ]; then
-    if [ ! -z ${SRCDS_APPID} ]; then
-	mkdir -p /home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}
-        export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/container/.steam/steam"
-        export STEAM_COMPAT_DATA_PATH="/home/container/.steam/steam/steamapps/compatdata/${SRCDS_APPID}"
-        export WINETRICKS="/usr/sbin/winetricks"
-        export STEAM_DIR="/home/container/.steam/steam/"
-    else
-        echo -e "----------------------------------------------------------------------------------"
-        echo -e "WARNING!!! Proton needs variable SRCDS_APPID, else it will not work. Please add it"
-        echo -e "Server stops now"
-        echo -e "----------------------------------------------------------------------------------"
-        exit 0
-        fi
+if [[ -f "/usr/local/bin/proton" ]] && ! setup_proton_compatibility; then
+    exit 0
 fi
 
 cd /home/container || exit 1
 
-if [ "${AUTO_UPDATE}" == "1" ]; then 
-    ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+if [ "${AUTO_UPDATE}" == "1" ]; then
+    STEAM_LOGIN="+login anonymous"
+    if [[ -n "${STEAM_USER:-}" && -n "${STEAM_PASS:-}" ]]; then
+        STEAM_LOGIN="+login \"${STEAM_USER}\" \"${STEAM_PASS}\""
+    fi
+
+    ./steamcmd/steamcmd.sh +force_install_dir /home/container "${STEAM_LOGIN}" $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
 else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
